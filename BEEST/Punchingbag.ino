d35fc6e5457d1a487da0ed865e9a2ae9838a66f5
AmuEmu <107953747+amna8977@users.noreply.github.com>
2026-05-22 13:23:33 -0700

    Add files via upload

---
+++ BEEST/Punchingbag.ino
@@ -0,0 +1,33 @@
+﻿#include <Arduino.h>
+
+const int buttonPin = 2;
+const unsigned long debounceDelay = 50;
+int lastButtonState = HIGH;
+int lastStableState = HIGH;
+unsigned long lastDebounceTime = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("The Zone bag is ready");
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != lastStableState) {
+      lastStableState = reading;
+      if (lastStableState == LOW) {
+        Serial.println("punch");
+      }
+    }
+  }
+
+  lastButtonState = reading;
+}
